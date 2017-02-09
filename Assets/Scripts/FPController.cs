9ace178b866811ffe70e5dddef3501f6705b797a
grreuze <grreuze@gmail.com>
2017-02-10 00:51:43 +0100

    cameraTests

--- Assets/Scripts/FPController.cs
+++ Assets/Scripts/FPController.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class FPController : MonoBehaviour {
 
     public Transform rotator; // Used to get the rotation of the camera
-    public float walkForce = 3, runForce = 6, hSpeed = 3, gravity = 10, fallToDeath = 3;
+    public float walkForce = 3, hSpeed = 3, gravity = 10, fallToDeath = 3;
 
     float xForce, zForce, distToGround, falling;
     Vector3 direction;
@@ -13,14 +13,6 @@ public class FPController : MonoBehaviour {
 
     bool isGrounded {
         get { return Physics.Raycast(transform.position, -Vector3.up, distToGround + .1f); }
-    }
-    float force {
-        get {
-            if (Input.GetMouseButton(1)) {
-                Cursor.lockState = CursorLockMode.Locked;
-                return runForce;
-            } else return walkForce;
-        }
     }
 
     void Start() {
@@ -45,8 +37,8 @@ public class FPController : MonoBehaviour {
             if (falling >= fallToDeath) ReloadScene();
         }
 
-        xForce = Input.GetAxis("Horizontal") * force;
-        zForce = Input.GetAxis("Vertical") * force;
+        xForce = Input.GetAxis("Horizontal") * walkForce;
+        zForce = Input.GetAxis("Vertical") * walkForce;
         
         if (xForce + zForce != 0) {
             transform.rotation = rotator.rotation;
--- Assets/Scripts/MouseOrbitImproved.cs
+++ Assets/Scripts/MouseOrbitImproved.cs
@@ -6,8 +6,7 @@ public class MouseOrbitImproved : MonoBehaviour {
     public Transform target;
 
     public float distance = 5.0f;
-    public float xSpeed = 120.0f;
-    public float ySpeed = 120.0f;
+    public float xSpeed = 15, ySpeed = 15;
 
     public float yMinLimit = -20f;
     public float yMaxLimit = 80f;
@@ -15,7 +14,12 @@ public class MouseOrbitImproved : MonoBehaviour {
     public float distanceMin = .5f;
     public float distanceMax = 15f;
 
-    private Rigidbody rigidbody;
+    public bool canZoom;
+
+    public float smooth = 4;
+
+    Vector3 camPosition;
+    Quaternion camRotation;
 
     float x = 0.0f;
     float y = 0.0f;
@@ -25,18 +29,18 @@ public class MouseOrbitImproved : MonoBehaviour {
         Vector3 angles = transform.eulerAngles;
         x = angles.y;
         y = angles.x;
-
-        rigidbody = GetComponent<Rigidbody>();
-
-        // Make the rigid body not change rotation
-        if (rigidbody != null) {
-            rigidbody.freezeRotation = true;
-        }
     }
 
     void LateUpdate() {
         if (target) {
-            x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+
+            if (Physics.Linecast(transform.position, transform.position + transform.right) || Physics.Linecast(transform.position, transform.position - transform.right)) {
+                distance -= Mathf.Abs(Input.GetAxis("Mouse X"));
+            } else {
+                x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+            }
+            
+
             y -= Input.GetAxis("Mouse Y") * ySpeed * distance * 0.02f;
 
             y = ClampAngle(y, yMinLimit, yMaxLimit);
@@ -47,17 +51,24 @@ public class MouseOrbitImproved : MonoBehaviour {
 
             RaycastHit hit;
             if (Physics.Linecast(target.position, transform.position, out hit)) {
-                //distance -= hit.distance;
-                distance -= 0.1f;
+                distance -= hit.distance;
             }
+            smoothMovement();
+
             Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
             Vector3 position = rotation * negDistance + target.position;
 
-            transform.rotation = rotation;
-            transform.position = position;
+            camRotation = rotation;
+            camPosition = position;
             
-            target.rotation = Quaternion.Euler(0, x, 0);
+            target.rotation = Quaternion.Euler(0, x, 0); // Reoriente the character's rotator
         }
+    }
+
+    void smoothMovement() {
+        float t = Time.deltaTime * smooth;
+        transform.position = Vector3.Lerp(transform.position, camPosition, t);
+        transform.rotation = Quaternion.Lerp(transform.rotation, camRotation, t);
     }
 
     public static float ClampAngle(float angle, float min, float max) {
